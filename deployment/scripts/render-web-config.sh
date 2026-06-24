5e456157f75ad4d2a7e3defd6db595ef2a571149
Mykyta Sydorenko <102351233+lilbonekit@users.noreply.github.com>
2026-06-24 16:53:16 +0300

    Fix price (#86)

--- deployment/scripts/render-web-config.sh
+++ deployment/scripts/render-web-config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-envsubst '${WEB_ESPLORA_API_UPSTREAM}' \
+envsubst '${WEB_ESPLORA_API_UPSTREAM} ${WEB_PRICES_API_UPSTREAM}' \
   < /deployment/configs/nginx/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
